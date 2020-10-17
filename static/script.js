a7fbf32ca8e3712b1d1278bf4d3e7e780439528c
Alexander Lindhe <allindhe@gmail.com>
2020-10-17 22:39:15 +0200

    Add coloring start and end cells. Also add foundation for extracting data for sending to backend

--- static/script.js
+++ static/script.js
@@ -1,7 +1,8 @@
 ROWS = 10;
 COLUMNS = 10;
+NCELLS = ROWS * COLUMNS;
 
-let mainTable = $("#main-table");
+let mainTbody = $("#main-table");
 
 // Populate main table with cells
 for (let row = 0; row < ROWS; row++){
@@ -10,5 +11,26 @@ for (let row = 0; row < ROWS; row++){
         html_string += "<td></td>";
     }
     html_string += "</tr>";
-    mainTable.append(html_string);
+    mainTbody.append(html_string);
 }
+
+// Temporarily set a permanent start and end
+$("#main-table tr td").first().toggleClass("start-cell")
+$("#main-table tr td").last().toggleClass("end-cell")
+
+// Get table info
+let data = {"start-cell": null,
+            "end-cell": null,
+            "walls": [],
+            "rows": ROWS,
+            "columns": COLUMNS};
+$("#main-table tr td").each((index, cell) => {
+    let classes = cell.classList;
+    if (classes.contains("start-cell")){
+        data["start-cell"] = [Math.floor(index / ROWS), index % COLUMNS]
+    }
+    else if (classes.contains("end-cell")){
+        data["end-cell"] = [Math.floor(index / ROWS), index % COLUMNS]
+    }
+});
+console.log(data)
