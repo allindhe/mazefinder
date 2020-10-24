4fbc4177ecb4ea0269f35ad0c487ed889b1fc357
Alexander Lindhe <allindhe@gmail.com>
2020-10-24 11:16:45 +0200

    Add a marker for current cell when visualizing

--- static/script.js
+++ static/script.js
@@ -1,6 +1,6 @@
 let ROWS = 30;
 let COLUMNS = 30;
-let VISUALIZE_MS = 5;
+let VISUALIZE_MS = 50;
 
 // Keep track on if mouse is down or up and current wall status when clicked
 let isMouseDown = false;
@@ -11,7 +11,7 @@ let mainTbody = $("#main-table");
 // Start stuff
 function clearCells(stepCell=true, wallCell=true){
     if (stepCell){
-        $("#main-table tr td").removeClass("step-cell")
+        $("#main-table tr td").removeClass("step-cell").removeClass("current-step-cell")
     }
     if (wallCell){
         $("#main-table tr td").removeClass("wall-cell")
@@ -20,12 +20,21 @@ function clearCells(stepCell=true, wallCell=true){
 function visualizeCells(data) {
     clearCells(stepCell=true, wallCell=false);
 
-    timeout = 0;
+    let timeout = 0;
+    let previous_cell = null;
     
     data["steps"].forEach(cell => {
         timeout += VISUALIZE_MS
-        setTimeout(function(){ 
-            mainTbody[0].rows[cell[0]].cells[cell[1]].classList.add("step-cell"); 
+        setTimeout(function(){
+            // Set class on previous cell
+            if (previous_cell){
+                previous_cell.classList.remove("current-step-cell");
+                previous_cell.classList.add("step-cell");
+            }
+
+            // Color current cell
+            mainTbody[0].rows[cell[0]].cells[cell[1]].classList.add("current-step-cell");
+            previous_cell = mainTbody[0].rows[cell[0]].cells[cell[1]];
         }, timeout);
     });
 }
@@ -33,7 +42,6 @@ function visualizeCells(data) {
 function calculateAlgorithm() {
     let inputData = getTableInfo();
     let outputData;
-    console.log(inputData)
 
     // Send board to backend and recieve solution
     $.ajax('/_algorithm?data=' + JSON.stringify(inputData),
@@ -41,7 +49,6 @@ function calculateAlgorithm() {
         dataType: 'json', // type of response data
         timeout: 500,     // timeout milliseconds
         success: function (outputData,status,xhr) {   // success callback function
-            console.log(outputData)
             visualizeCells(outputData);
         },
         error: function (jqXhr, textStatus, errorMessage) { // error callback 
